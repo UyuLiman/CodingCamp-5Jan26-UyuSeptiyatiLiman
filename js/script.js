29d87ce7fca3fa273dd217dee4574b7174c83624
UyuLiman <uyuliman@gmail.com>
2026-01-12 00:53:41 +0700

    Konfigurasi mini

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Hello Uyu");
+//Temporary Storage for Todo Items
+let todo = [];
+
+//Function to Add Todo Item
+function addTodo() { 
+    const todoInput = document.getElementById('todo-input');
+    const todoDate = document.getElementById('todo-date');
+
+    console.log(todoInput);
+    console.log(todoDate);
+}
+
+//Fuction to Remove All Todo Items
+function removeAllTodo() { }
+
+//Function to Filter Todo Items
+function filterTodo() { }
