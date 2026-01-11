ff536da2680ac98035399188ae09668bfaeda613
UyuLiman <uyuliman@gmail.com>
2026-01-12 01:22:01 +0700

    Konfigurasi mini

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 //Temporary Storage for Todo Items
-let todo = [];
+let todos = [];
 
 //Function to Add Todo Item
 function addTodo() { 
     const todoInput = document.getElementById('todo-input');
     const todoDate = document.getElementById('todo-date');
+
+    if (todoInput.value === '' || todoDate.value === '') {
+        alert('Please enter both todo item and due date.');
+    } else {
+        const newTodo = {
+            item: todoInput.value,
+            date: todoDate.value
+        };
+        todo.push(newTodo);
+
+        console.log('Todo item added:', todos);
 
     console.log(todoInput.value);
     console.log(todoDate.value);
